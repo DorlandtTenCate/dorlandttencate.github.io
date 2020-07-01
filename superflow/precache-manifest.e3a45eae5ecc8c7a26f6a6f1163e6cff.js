83202d61cab3176a2426fa3701cd88ce3ca1aded
Pelle ten Cate <pelle@technoot.nl>
2020-07-01 07:32:01 -0500

    feat: add superflow

---
+++ superflow/precache-manifest.e3a45eae5ecc8c7a26f6a6f1163e6cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92356902e67416d2dd19a6472fb3033",
+    "url": "/superflow/index.html"
+  },
+  {
+    "revision": "3148b2dc9e9bc179bcd6",
+    "url": "/superflow/static/css/main.410c97d0.chunk.css"
+  },
+  {
+    "revision": "cbae63ea378492b27477",
+    "url": "/superflow/static/js/2.5a8a1026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superflow/static/js/2.5a8a1026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3148b2dc9e9bc179bcd6",
+    "url": "/superflow/static/js/main.549d80c9.chunk.js"
+  },
+  {
+    "revision": "8b7dff0592daa3ad1dcf",
+    "url": "/superflow/static/js/runtime-main.492092d4.js"
+  }
+]);
