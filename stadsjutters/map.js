c662e1b5df545f13570ad69e234a4192096d1062
Pelle ten Cate <pelletencate@gmail.com>
2020-12-06 18:58:05 +0100

    feat: match colors with Airtable

--- stadsjutters/map.js
+++ stadsjutters/map.js
@@ -14,13 +14,17 @@ async function loadMapData() {
   const json = await res.json();
 
   json.records.forEach((record) => {
+    const radius = Number(record.fields.radius.match(/\((\d+)m\)/)[1]);
+    const colors = { 25: '#102046', 75: '#04283f', 250: '#012524', 500: '#0b1d05' };
+    const fillColors = { 25: '#cfdfff', 75: '#d0f0fd', 250: '#c2f5e9', 500: '#d1f7c4' };
+    debugger;
     L.circle([record.fields.Latitude, record.fields.Longitude], {
-      color: '#084',
-      fillColor: '#0f8',
-      fillOpacity: 0.3,
+      color: colors[radius],
+      fillColor: fillColors[radius],
+      fillOpacity: 0.5,
       radius: Number(record.fields.radius.match(/\((\d+)m\)/)[1]),
       stroke: true,
-      weight: 2,
+      weight: 1,
     }).addTo(map);
   });
 }
