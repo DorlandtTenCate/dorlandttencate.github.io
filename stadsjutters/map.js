854a5c1ec6a5b127da38bf29f46ac7c97585a07d
Pelle ten Cate <pelletencate@gmail.com>
2020-12-06 19:07:44 +0100

    feat: make circles stand out more

--- stadsjutters/map.js
+++ stadsjutters/map.js
@@ -17,11 +17,10 @@ async function loadMapData() {
     const radius = Number(record.fields.radius.match(/\((\d+)m\)/)[1]);
     const colors = { 25: '#102046', 75: '#04283f', 250: '#012524', 500: '#0b1d05' };
     const fillColors = { 25: '#cfdfff', 75: '#d0f0fd', 250: '#c2f5e9', 500: '#d1f7c4' };
-    debugger;
     L.circle([record.fields.Latitude, record.fields.Longitude], {
       color: colors[radius],
       fillColor: fillColors[radius],
-      fillOpacity: 0.5,
+      fillOpacity: 0.3,
       radius: Number(record.fields.radius.match(/\((\d+)m\)/)[1]),
       stroke: true,
       weight: 1,
